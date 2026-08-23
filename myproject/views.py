6be318a9e6a8fb027caec8700042cfb97ee9d8b6
Sharvari Kapse <sharvarikapse16@gmail.com>
2026-08-24 00:36:52 +0530

    update views

--- myproject/views.py
+++ myproject/views.py
@@ -8,3 +8,19 @@ def shop(request):
 
 def cart(request):
     return render(request , 'cart.html')
+
+def categories(request):
+    return render(request , 'categories.html')
+
+def offers(request):
+    return render(request , 'offers.html')
+
+def about(request):
+    return render(request , 'about.html')
+
+def contact(request):
+    return render(request , 'contact.html')
+
+def login(request):
+    return render(request , 'login.html')
+
