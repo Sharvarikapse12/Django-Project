89a9ae6c91f8c77830c594f79c095d97bad55f46
Sharvari Kapse <sharvarikapse16@gmail.com>
2026-08-23 23:56:26 +0530

    commit all

--- myproject/views.py
+++ myproject/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request , 'home.html')
+
+def shop(request):
+    return render(request , 'shop.html')
+
+def cart(request):
+    return render(request , 'cart.html')
