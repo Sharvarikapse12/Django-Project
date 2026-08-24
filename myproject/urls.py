a1b83238aa476376f597bce356d5284ea1374276
Sharvari Kapse <sharvarikapse16@gmail.com>
2026-08-24 22:44:37 +0530

    commit done

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from . import views
 
 urlpatterns = [
@@ -28,5 +28,6 @@ urlpatterns = [
     path('about/' , views.about),
     path('contact/' , views.contact),
     path('login/' , views.login),
-    path('wishlist' , views.wishlist)
+    path('wishlist' , views.wishlist),
+    path('cart/payment' , include('payment.urls'))
 ]
